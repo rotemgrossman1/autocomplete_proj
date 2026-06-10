370b40d94fdd4cb2aab23f7f1f3b4447e99cdf10
rotemgrossman1 <rotem.grossman@live.biu.ac.il>
2026-06-10 10:45:05 +0300

    refactor: simplify searchChildren method and update its usage in addWord and findWord

--- AutoCompleteTrie.js
+++ AutoCompleteTrie.js
@@ -5,8 +5,8 @@ class AutoCompleteTrie{
         this.endOfWord = false
     }
 
-    searchChildren(node, letter){
-        return node.children[letter] || null// Returns the child node for the given letter, or null if it doesn't exist
+    searchChildren(letter) {
+        return this.children[letter] || null; 
     }
     addWord(word){
         let n = word.length;
@@ -14,7 +14,7 @@ class AutoCompleteTrie{
         let currentNode = this
         while(i < n){
             
-            let nextNode = this.searchChildren(currentNode, word[i])
+            let nextNode = currentNode.searchChildren(word[i])
             if(nextNode!==null){//this means the next letter in word is aleady in our tire
                 i++;
                 currentNode = nextNode
@@ -39,7 +39,7 @@ class AutoCompleteTrie{
     findWord(word){
         let currentNode = this
         for(let letter of word){
-            let nextNode = this.searchChildren(currentNode, letter)
+            let nextNode = currentNode.searchChildren(letter);
             if(nextNode!==null){//this means the next letter in word is aleady in our tire
                 currentNode = nextNode
             }
@@ -53,10 +53,10 @@ class AutoCompleteTrie{
     }
     // Navigates to the last node of the given prefix
     // Returns the node where the prefix ends
-    _getRemainingTree(prefix, node){
-        let currentNode = this
+    _getRemainingTree(prefix, node = this){
+        let currentNode = node
         for(let letter of prefix){
-            let nextNode = this.searchChildren(currentNode, letter)
+            let nextNode = currentNode.searchChildren(letter);
             if(nextNode!==null){//this means the next letter in word is aleady in our tire
                 currentNode = nextNode
             }
